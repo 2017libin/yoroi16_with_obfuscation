f4df8dc7375a50b5a582b3cf15e6a3dc96949a7f
Chase51 <1963482624@qq.com>
2023-12-07 09:36:31 +0800

    rename wb_yoroi_poly1 to wb_yoroi16_poly1

--- tigress_poly1.sh
+++ tigress_poly1.sh
@@ -3,7 +3,7 @@ tigress --Seed=66 --Statistics=0 --Verbosity=1 --Environment=x86_64:Linux:Gcc:4.
    --Transform=EncodeData \
       --GlobalVariables='T1,T2,T3,T1_inv,T2_inv,T3_inv' \
       --EncodeDataCodecs=poly1 \
-   --out=./wb_yoroi_poly1.c ./wb_yoroi16.c
+   --out=./wb_yoroi16_poly1.c ./wb_yoroi16.c
 
 # ./a.out
 
